dd6fd817b5a5a67bae951f8aaebcdd1b74f200e2
Vaindzi <neuralidz@gmail.com>
2024-05-09 17:59:08 -0600

    cambios en los botones

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ import streamlit as st
 
 car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
 
-st.header('Lanzar una moneda')  # poner un encabezado
+# poner un encabezado
+st.header('Aplicación de Visualización de Datos de Venta de Vehículos')
 
-start_button = st.button('Construir histograma')  # construir un boton
+hist_button = st.button('Construir histograma')  # construir un boton
 
-if start_button:  # al hacer clic en el botón
+if hist_button:  # al hacer clic en el botón
     # escribir un mensaje
     st.write(
         'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
@@ -23,7 +24,7 @@ st.plotly_chart(fig, use_container_width=True)
 disp_button = st.button(
     'Construir gráfico de dispersión')  # construir un boton
 
-if start_button:  # al hacer clic en el botón
+if disp_button:  # al hacer clic en el botón
     # escribir un mensaje
     st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
 
